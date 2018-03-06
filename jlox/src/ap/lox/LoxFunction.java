e560fed008e4c3bd213aaf395b1ae80e52105335
a <a>
2018-03-06 14:30:18 +0800

    Chapter 10 done

---
+++ jlox/src/ap/lox/LoxFunction.java
@@ -0,0 +1,43 @@
+package ap.lox;
+
+import com.sun.tools.doclint.Env;
+
+import java.util.List;
+
+/**
+ *
+ */
+public class LoxFunction implements LoxCallable {
+    private final Stmt.Function declaration;
+    private final Environment closure;
+
+    LoxFunction(Stmt.Function declaration, Environment closure) {
+        this.declaration = declaration;
+        this.closure = closure;
+    }
+
+    @Override
+    public int arity() {
+        return declaration.parameters.size();
+    }
+
+    @Override
+    public Object call(Interpreter interpreter, List<Object> arguments) {
+        Environment environment = new Environment(closure);
+        for (int i = 0; i < declaration.parameters.size(); ++i) {
+            environment.define(declaration.parameters.get(i).lexeme, arguments.get(i));
+        }
+
+        try {
+            interpreter.executeBlock(declaration.body, environment);
+        } catch (Return returnValue) {
+            return returnValue.value;
+        }
+        return null;
+    }
+
+    @Override
+    public String toString() {
+        return "<fn " + declaration.name.lexeme + ">";
+    }
+}
---
+++ jlox/src/ap/lox/RuntimeError.java
@@ -0,0 +1,13 @@
+package ap.lox;
+
+/**
+ *
+ */
+public class RuntimeError extends RuntimeException {
+    final Token token;
+
+    RuntimeError(Token token, String message) {
+        super(message);
+        this.token = token;
+    }
+}
